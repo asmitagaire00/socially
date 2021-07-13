56e4857690162c8bc07ff001df78f0d8eb5f9306
kishor <kishorliv@gmail.com>
2021-07-13 15:31:48 +1000

    Fix linting.

--- babel.config.js
+++ babel.config.js
@@ -5,13 +5,13 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         targets: {
           esmodules: true,
         },
       },
     ],
-    "@babel/preset-react",
+    '@babel/preset-react',
   ],
 };
