c213a333b94ea60a56a1197b11db90ce08589224
kishor <kishorliv@gmail.com>
2021-07-14 17:01:16 +1000

    Set historyApiFallback to true to allow webpack to serve app on any path.

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,48 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.join(__dirname, "src", "public", "js", "main.jsx"),
-    output: {
-        path: path.join(__dirname, "dist"),
-        publicPath: "/",
-        filename: "bundle.js",
-    },
-    resolve: {
-        extensions: [".jsx", ".js"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-react"],
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader", "postcss-loader"],
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [
-                    {
-                        loader: "file-loader",
-                    },
-                ],
-            },
+  entry: path.join(__dirname, 'src', 'public', 'js', 'main.jsx'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(
-                __dirname,
-                "src",
-                "public",
-                "views",
-                "index.html"
-            ),
-        }),
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'public', 'views', 'index.html'),
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
 };
