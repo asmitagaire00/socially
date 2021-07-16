ff5893404d132192f26b8efa15196b3cf2d8236c
kishor <kishorliv@gmail.com>
2021-07-16 23:30:31 +1000

    Add redux store.

--- src/public/js/redux/store.js
+++ src/public/js/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import alertReducer from '../components/Alert/AlertSlice';
+import authReducer from './AuthSlice';
 
 const store = configureStore({
   reducer: {
     alert: alertReducer,
+    auth: authReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
