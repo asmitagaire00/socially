ef0b2c1925272518e13e820e6be12423fda74638
kishor <kishorliv@gmail.com>
2021-07-31 22:56:29 +1000

    Add form validation.

--- src/public/js/components/Login/Login.jsx
+++ src/public/js/components/Login/Login.jsx
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
+
 import routes from '../../config/routes';
 import { login } from '../../redux/AuthSlice';
+import validate from './validate';
 
-export default function Login() {
+function renderField({ input, label, type, meta: { touched, error } }) {
+  return (
+    <div>
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label>{label}</label>
+      <div>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    </div>
+  );
+}
+
+function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -22,28 +40,39 @@ export default function Login() {
   return (
     <>
       {isAuthenticated && <Redirect to={`${routes.root}`} />}
-      <div>
-        <form onSubmit={onSubmit}>
-          <input
-            type="email"
-            placeholder="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button type="submit">Login</button>
-        </form>
-      </div>
+
+      <form onSubmit={onSubmit}>
+        <Field
+          name="email"
+          type="email"
+          component={renderField}
+          label="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Field
+          name="password"
+          type="password"
+          component={renderField}
+          label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div>
+          <button type="submit" disabled={loading}>
+            Login
+          </button>
+          <Link to={`${routes.register}`}>Register</Link>
+        </div>
+      </form>
 
       {error && <p>{`Error: ${errorMessage}`}</p>}
       {loading && <p>Logging in...</p>}
     </>
   );
 }
+
+export default reduxForm({
+  form: 'loginForm',
+  validate,
+})(Login);
