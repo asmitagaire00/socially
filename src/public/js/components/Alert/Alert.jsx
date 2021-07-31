748eee690f6b4803a30063ebd12fbf45cccc479c
kishor <kishorliv@gmail.com>
2021-07-31 22:49:47 +1000

    Refactor Alert to handle generic notifications instead of just errors.

--- src/public/js/components/Alert/Alert.jsx
+++ src/public/js/components/Alert/Alert.jsx
@@ -7,28 +7,26 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import { hideError } from '../../redux/ErrorNotificationSlice';
+import { hideNotification } from '../../redux/NotificationSlice';
 
 export default function Alert() {
-  const isOpen = useSelector((state) => state.errorNotification.isOpen);
-  const errorMessage = useSelector(
-    (state) => state.errorNotification.errorMessage,
-  );
+  const isOpen = useSelector((state) => state.notification.isOpen);
+  const message = useSelector((state) => state.notification.message);
 
   const dispatch = useDispatch();
 
   function handleClose() {
-    dispatch(hideError());
+    dispatch(hideNotification());
   }
 
   return (
     <>
-      {isOpen && errorMessage && (
+      {isOpen && message && (
         <Dialog fullWidth open={isOpen} onClose={handleClose} data-test="alert">
           <DialogTitle data-test="alert__title">Error!</DialogTitle>
           <DialogContent>
             <DialogContentText data-test="alert__text">
-              {errorMessage}
+              {message}
             </DialogContentText>
           </DialogContent>
           <DialogActions>
