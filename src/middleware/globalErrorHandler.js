2d4c68952298a7d286f4e1bb8026e66cf284fea3
kishor <kishorliv@gmail.com>
2021-08-14 23:46:57 +1000

    Handle mongoose error and catch error if it cannot be mapped to ApplicationError.

--- src/middleware/globalErrorHandler.js
+++ src/middleware/globalErrorHandler.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const ApplicationError = require('../lib/error/ApplicationError');
 const CommonError = require('../lib/error/commonErrors');
 const formatError = require('../lib/error/formatError');
@@ -12,9 +14,27 @@ function globalErrorHandler(err, req, res, next) {
     return res.status(err.statusCode || 500).json(formatError(err));
   }
 
+  // handle errors related to mongodb
+  if (err instanceof mongoose.Error) {
+    console.log(err);
+    const internalError = new ApplicationError(
+      CommonError.INTERNAL_SERVER_ERROR,
+    );
+    return res.status(err.statusCode || 500).json(formatError(internalError));
+  }
+
   if (err instanceof Error) {
-    const newError = mapToApplicationError(err);
-    return res.status(err.statusCode || 500).json(formatError(newError));
+    let unknownError;
+    try {
+      const newError = mapToApplicationError(err);
+      return res.status(err.statusCode || 500).json(formatError(newError));
+    } catch (error) {
+      // log error
+      console.log(error);
+    } finally {
+      unknownError = new ApplicationError(CommonError.UNKNOWN_ERROR);
+    }
+    return res.status(err.statusCode || 500).json(formatError(unknownError));
   }
 
   const unknownError = new ApplicationError(CommonError.UNKNOWN_ERROR);
