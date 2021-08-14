d917978dbc6517f1864878f74908e6fe567ac6b9
kishor <kishorliv@gmail.com>
2021-08-14 23:49:26 +1000

    Set useFindAndModify option to true.

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose');
 
 const Account = require('../api/v1/account/account.model');
 const RefreshToken = require('../api/v1/account/refresh-token.model');
+const Follow = require('../api/v1/follow/follow.model');
 
+// eslint-disable-next-line no-use-before-define
+module.exports = { connectDB, Account, RefreshToken, Follow };
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 function connectDB() {
-  return mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true,
-  });
+  return mongoose.connect(process.env.MONGO_URI);
 }
-
-module.exports = { connectDB, Account, RefreshToken };
