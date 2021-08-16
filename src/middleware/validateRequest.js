18cc2099a6df61cdb8c2a301294304b3369967ed
kishor <kishorliv@gmail.com>
2021-08-17 02:33:24 +1000

    Allow schemas other than 'req.body' to be validated against.

--- src/middleware/validateRequest.js
+++ src/middleware/validateRequest.js
@@ -2,15 +2,21 @@ const mapToApplicationError = require('../lib/error/mapToApplicationError');
 
 /* eslint-disable no-use-before-define */
 module.exports = validateRequest;
-
-function validateRequest(req, next, joiSchema) {
+/**
+ *
+ * @param {req} req express req obj
+ * @param {next} next express next
+ * @param {object} joiSchema joi schema object
+ * @param {object} valSchema (optional) schema to be validated instead of req.body
+ */
+function validateRequest(req, next, joiSchema, valSchema) {
   const options = {
     abortEarly: false,
     allowUnknown: true,
     stripUnknown: true,
   };
 
-  const { error, value } = joiSchema.validate(req.body, options);
+  const { error, value } = joiSchema.validate(valSchema || req.body, options);
 
   if (error) {
     const err = mapToApplicationError(error);
