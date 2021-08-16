5ae6c1f2e81f66925085dc643ad008434330242f
kishor <kishorliv@gmail.com>
2021-08-16 17:23:28 +1000

    Import Follow model.

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const Account = require('../api/v1/account/account.model');
 const RefreshToken = require('../api/v1/account/refresh-token.model');
 const Follow = require('../api/v1/follow/follow.model');
+const User = require('../api/v1/user/user.model');
 
 // eslint-disable-next-line no-use-before-define
-module.exports = { connectDB, Account, RefreshToken, Follow };
+module.exports = { connectDB, Account, RefreshToken, Follow, User };
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
