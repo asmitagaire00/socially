a5a03e2d9eb40b80b65f2c739378bd45fc507f02
kishor <kishorliv@gmail.com>
2021-08-19 20:46:14 +1000

    Add test to validate the creation of new post.

--- src/api/tests/integration/api/v1/post/post.test.js
+++ src/api/tests/integration/api/v1/post/post.test.js
@@ -6,8 +6,7 @@ const setupTestDB = require('../../../../utils/setupTestDB');
 const db = require('../../../../../../helpers/db');
 const {
   createAccounts,
-  accountOne,
-  accountOneJwtToken,
+  accountVerifiedOne,
 } = require('../../../../fixtures/account.fixture');
 
 // setup test database and teardown processes
@@ -19,28 +18,37 @@ describe('Post routes', () => {
   beforeEach(() => {
     newPost = {
       caption: faker.name.findName(),
-      image: faker.name.findName(),
-      user: faker.name.findName(),
     };
   });
 
-  // describe('POST /post/create', () => {
-  //   it('should return 200 ok and successfully create new post if data is valid', async () => {
-  //     await createAccounts([accountOne]);
+  describe('POST /post/create', () => {
+    it('should return 200 ok and successfully create new post if data is valid', async () => {
+      const accounts = await createAccounts([accountVerifiedOne]);
+      const bearerTokenOne = accounts[0].jwtToken;
 
-  //     const res = await supertest(app)
-  //       .post('/api/v1/post/create')
-  //       .set('Authorization', `Bearer ${accountOneJwtToken}`)
-  //       .send(newPost)
-  //       .expect(200);
+      const res = await supertest(app)
+        .post('/api/v1/post/create')
+        .set('Authorization', `Bearer ${bearerTokenOne}`)
+        .send(newPost)
+        .expect(200);
 
-  //     expect(res.body).toMatchObject({
-  //       message: expect.anything(),
-  //       success: true,
-  //     });
+      expect(res.body).toEqual({
+        data: {
+          tags: expect.anything(),
+          likes: expect.anything(),
+          comments: expect.anything(),
+          caption: expect.anything(),
+          user: expect.anything(),
+          createdAt: expect.anything(),
+          updatedAt: expect.anything(),
+          id: expect.anything(),
+        },
+        message: expect.anything(),
+        success: true,
+      });
 
-  //     const dbUser = await db.Post.findOne({ user: newPost.user });
-  //     expect(dbUser).toBeDefined();
-  //   });
-  // });
+      const dbPost = await db.Post.findOne({ id: res.body.data.id });
+      expect(dbPost).toBeDefined();
+    });
+  });
 });
