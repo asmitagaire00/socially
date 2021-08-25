af18902b429c3f12e59796606e93144ee6e0e564
kishor <kishorliv@gmail.com>
2021-08-25 23:42:06 +1000

    Set mongoose parameters directly using set() due to issues.

--- src/api/tests/utils/setupTestDB.js
+++ src/api/tests/utils/setupTestDB.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
 const setupTestDB = () => {
   beforeAll(async () => {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
   });
 
   beforeEach(async () => {
