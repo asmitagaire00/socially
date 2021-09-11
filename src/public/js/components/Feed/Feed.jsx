77e8f55a2514eb3ef76573627f6df400aa4a9ee3
kishor <kishorliv@gmail.com>
2021-09-11 19:37:04 +1000

    List posts and use inifinite scrolling.

--- src/public/js/components/Feed/Feed.jsx
+++ src/public/js/components/Feed/Feed.jsx
@@ -1,14 +1,74 @@
-import React from "react";
+import React, { useCallback, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPosts } from '../../redux/PostSlice';
 
-import AddPost from "../AddPost";
-import Post from "../Post";
+import AddPost from '../AddPost';
+import Post from '../Post';
 
 export default function Feed() {
-    return (
-        <div className="feed">
-            <AddPost />
-            <Post />
-            <Post />
-        </div>
-    );
+  const LIMIT = 7; // num of posts per request
+  const skip = useRef(0);
+  const dispatch = useDispatch();
+  const { loading, posts, totalPostsCount } = useSelector(
+    (state) => state.post,
+  );
+
+  const observer = useRef(null);
+
+  const bottomRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          dispatch(getPosts({ skip: skip.current, limit: LIMIT }));
+          skip.current += LIMIT;
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [loading, dispatch],
+  );
+
+  return (
+    <div className="feed">
+      <AddPost />
+      {posts &&
+        posts.map((post) => {
+          const {
+            likes,
+            comments,
+            tags,
+            caption,
+            image,
+            user,
+            id: postId,
+            createdAt,
+            updatedAt,
+          } = post;
+
+          return (
+            <Post
+              key={postId}
+              likes={likes}
+              comments={comments}
+              tags={tags}
+              caption={caption}
+              image={image}
+              user={user}
+              postId={postId}
+              createdAt={createdAt}
+              updatedAt={updatedAt}
+            />
+          );
+        })}
+      {posts.length === totalPostsCount ? (
+        <></>
+      ) : (
+        <div ref={bottomRef}>Loading...</div>
+      )}
+    </div>
+  );
 }
