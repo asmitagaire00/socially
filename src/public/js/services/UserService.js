a3b9fd64974b0439b0dcaa6573a1af98d73da49b
kishor <kishorliv@gmail.com>
2021-09-30 00:19:06 +1000

    Add service to follow/unfollow user

--- src/public/js/services/UserService.js
+++ src/public/js/services/UserService.js
@@ -1,71 +1,31 @@
-/* eslint-disable no-use-before-define */
 import axios from 'axios';
 import config from '../config/config';
 
-// eslint-disable-next-line import/prefer-default-export
-export const userService = {
-  register,
-  login,
-  autoLogin,
-  logout,
-  verifyEmail,
-  refreshToken,
+function followUser({ id, curUserId }) {
+  return axios({
+    method: 'POST',
+    withCredentials: true,
+    url: `${config.apiUrl}/api/v1/follows/${id}/follow`,
+    data: {
+      curUserId,
+    },
+  });
+}
+
+function unfollowUser({ id, curUserId }) {
+  return axios({
+    method: 'PUT',
+    withCredentials: true,
+    url: `${config.apiUrl}/api/v1/follows/${id}/unfollow`,
+    data: {
+      curUserId,
+    },
+  });
+}
+
+const userService = {
+  followUser,
+  unfollowUser,
 };
 
-async function register(user) {
-  return axios({
-    method: 'POST',
-    withCredentials: true, // request browser to set the cookie
-    url: `${config.apiUrl}/api/v1/accounts/register`,
-    data: user,
-  });
-}
-
-async function login(email, password) {
-  return axios({
-    method: 'POST',
-    withCredentials: true, // request browser to set the cookie
-    url: `${config.apiUrl}/api/v1/accounts/login`,
-    data: { email, password },
-  });
-}
-
-async function autoLogin(token) {
-  return axios({
-    method: 'POST',
-    withCredentials: true, // request browser to set the cookie
-    url: `${config.apiUrl}/api/v1/accounts/auto-login`,
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-}
-
-async function logout(token) {
-  // 'refreshToken' cookie is sent
-  return axios({
-    method: 'POST',
-    withCredentials: true, // request browser to set the cookie
-    url: `${config.apiUrl}/api/v1/accounts/revoke-token`,
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-}
-
-async function verifyEmail(emailVerificationToken) {
-  return axios({
-    method: 'POST',
-    withCredentials: true, // request browser to set the cookie
-    url: `${config.apiUrl}/api/v1/accounts/verify-email`,
-    data: { emailVerificationToken },
-  });
-}
-
-async function refreshToken() {
-  return axios({
-    method: 'POST',
-    withCredentials: true, // request browser to set the cookie
-    url: `${config.apiUrl}/api/v1/accounts/refresh-token`,
-  });
-}
+export default userService;
