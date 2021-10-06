280c7bafaf165457e52f6f0b5ed39d8a74104287
kishor <kishorliv@gmail.com>
2021-10-06 19:38:39 +1100

    Change alert component timed.

--- src/public/js/components/Alert/Alert.jsx
+++ src/public/js/components/Alert/Alert.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import Dialog from '@material-ui/core/Dialog';
-import Button from '@material-ui/core/Button';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContentText from '@material-ui/core/DialogContentText';
+import MuiAlert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
+
 import { hideNotification } from '../../redux/NotificationSlice';
 
 export default function Alert() {
   const isOpen = useSelector((state) => state.notification.isOpen);
+  const isError = useSelector((state) => state.notification.isError);
   const message = useSelector((state) => state.notification.message);
 
   const dispatch = useDispatch();
@@ -22,25 +20,15 @@ export default function Alert() {
   return (
     <>
       {isOpen && message && (
-        <Dialog fullWidth open={isOpen} onClose={handleClose} data-test="alert">
-          <DialogTitle data-test="alert__title">Error!</DialogTitle>
-          <DialogContent>
-            <DialogContentText data-test="alert__text">
-              {message}
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button
-              variant="contained"
-              onClick={handleClose}
-              color="primary"
-              autoFocus
-              data-test="alert__button"
-            >
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
+        <Snackbar open={isOpen} autoHideDuration={5000} onClose={handleClose}>
+          <MuiAlert
+            variant="filled"
+            onClose={handleClose}
+            severity={isError ? 'error' : 'info'}
+          >
+            {message}
+          </MuiAlert>
+        </Snackbar>
       )}
     </>
   );
